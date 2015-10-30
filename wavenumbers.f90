ba0a0579c82efea7bef0f40186aca1ed7ada055c
Omer San <omersan@gmail.com>
2015-10-30 11:07:14 -0500

    Update wavenumbers.f90

--- wavenumbers.f90
+++ wavenumbers.f90
@@ -27,14 +27,14 @@ np = 2000 !use 2000 points to plot curve
 open(12, file="wanenumbers.plt")
 write(12,*)'variables ="hk","c2","c4","c6","Pade4","Pade6","exact"'
 	do j=0,np
-		hk = dfloat(j)*pi/dfloat(np)
-		c2 = dsin(hk)
+	hk = dfloat(j)*pi/dfloat(np)
+	c2 = dsin(hk)
         c4 = (8.0d0*dsin(hk) - dsin(2.0d0*hk))/6.0d0
         c6 = (45.0d0*dsin(hk) - 9.0d0*dsin(2.0d0*hk) + dsin(3.0d0*hk) )/30.0d0
         p4 = 3.0d0*dsin(hk)/(2.0d0 + dcos(hk))
         p6 = (14.0d0/3.0d0*dsin(hk) + 1.0d0/6.0d0*dsin(2.0d0*hk))/(3.0d0 + 2.0d0*dcos(hk))
         
-		write(12,*) hk,c2,c4,c6,p4,p6,hk
+	write(12,*) hk,c2,c4,c6,p4,p6,hk
         
 	end do
 close(12)
